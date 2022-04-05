8ecba6db190fdd57d2c82d755419a2d591617bf1
damu <damu@inveniacorp.com>
2022-04-05 17:36:57 +0900

    init

---
+++ src/main/java/com/invenia/portalservice/PortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.invenia.portalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PortalServiceApplication.class, args);
+  }
+
+}
